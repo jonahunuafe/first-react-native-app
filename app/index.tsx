d5bacd4013a308760a0c98ba62c7ad150e381a2a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-12-02 16:33:33 +0100

    add not-found page

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
@@ -6,6 +7,9 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Page
+      </Link>
     </View>
   );
 }
@@ -20,4 +24,8 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff'
   },
+  button: {
+    textDecorationLine: "underline",
+    fontSize: 20,
+  }
 });
