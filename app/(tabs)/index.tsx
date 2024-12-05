1639235b3236512503d09cdc52817bb7bf7de87b
Jonah Unuafe <fejisfejiro@gmail.com>
2024-12-05 23:54:39 +0100

    style button dynamically

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
-import { Image } from "expo-image";
+import ImageViewer from '@/components/ImageViewer';
+import Button from "@/components/Button"
 
 const PlaceholderImage = require("@/assets/images/Ivory-billed-Woodpecker.jpg")
 
@@ -10,7 +10,11 @@ export default function Index() {
       style={styles.container}
     >
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -24,10 +28,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  }
 });
