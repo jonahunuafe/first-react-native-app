2ca572d47fe25bcdbf81303b8908458b423497c2
Jonah Unuafe <fejisfejiro@gmail.com>
2024-12-13 08:54:30 +0100

    add emoji's to image folder

---
+++ components/EmojiList.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { StyleSheet, FlatList, Platform, Pressable } from "react-native";
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+  onSelect: (image: ImageSource) => void;
+  onCloseModal: () => void;
+};
+
+export default function EmojiList({ onSelect, onCloseModal }: Props) {
+  const [emoji] = useState<ImageSource[]>([
+    
+  ]);
+
+  return (
+    <FlatList 
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      data={}
+      contentContainerStyle={styles.listContainer}
+
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
