1666b46f65a2a72008c26834b6c7ef5a51023c07
Jonah Unuafe <fejisfejiro@gmail.com>
2024-12-14 08:23:41 +0100

    implement functionality to capture screenshot on the web and save it as image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from "expo-image-picker";
 import { useState, useRef } from 'react';
 import { captureRef } from "react-native-view-shot";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from "expo-media-library";
+import domtoimage from "dom-to-image";
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -59,18 +60,35 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri= await captureRef(imageRef, {
-        height: 440,
-        quality: 1
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if(localUri) {
-        alert("Saved!");
+    if (Platform.OS !== "web") {
+      try {
+        const localUri= await captureRef(imageRef, {
+          height: 440,
+          quality: 1
+        });
+  
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if(localUri) {
+          alert("Saved!");
+        }
+      } catch(e) {
+        console.log(e);
       }
-    } catch(e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "Ivory-billed-Woodpecker.jpg";
+        link.href = dataUrl;
+        link.click();
+      } catch(e) {
+        console.log(e);
+      }
     }
   };
 
